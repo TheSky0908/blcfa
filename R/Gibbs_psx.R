16e9dc16993a4002d2e54ed4c452e540c2020869
TheSky0908 <12111603@mail.sustech.edu.cn>
2024-04-18 11:09:40 +0800

    Update Gibbs_psx.R

--- R/Gibbs_psx.R
+++ R/Gibbs_psx.R
@@ -82,7 +82,7 @@ sigly<-0.25
 #Inverse prior variance of unknown parameters in intercept
 sigmu<-0.25
 
-rou.scale<-0.1
+rou.scale<-100
 
 #rho_0, hyperparameters of Wishart distribution
 rou.zero<-rou.scale+NZ+1
